3a4cc4df453844f134e568312a06104c39154a3c
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2025-01-07 16:18:21 -0300

    video03: initial commit of the project

---
+++ anome-service/src/test/java/com/tallyto/anime_service/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tallyto.anime_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
